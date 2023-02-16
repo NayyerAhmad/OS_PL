2e573e4e4c727277813977bf3bc894e10c568279
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-16 23:58:52 +0200

    compatibility now has operating system dropdown

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,45 @@
-import React from "react";
-import CustomizedDialogs from "../components/EditLanguagePopoup";
-import FormPL from '../components/FormPL';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
+export default function SelectVariants() {
+  const [operatingSystem, setOperatingSystem] = React.useState('');
+  const [osNames, setOsNames] = React.useState([]);
 
-function App() {
+  React.useEffect(() => {
+    fetch('http://localhost:3001/eligibility/list')
+      .then(response => response.json())
+      .then(data => setOsNames(data.data.map(item => item.name_os)))
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = (event) => {
+    setOperatingSystem(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <CustomizedDialogs>
-        <FormPL></FormPL>
-      </CustomizedDialogs>
+    <div>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
+        <InputLabel id="OS-label">Operating System</InputLabel>
+        <Select
+          labelId="OS"
+          id="os_name"
+          value={operatingSystem}
+          onChange={handleChange}
+          label="Operating System"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {osNames.map(name => (
+            <MenuItem key={name} value={name}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
-  )
+  );
 }
-
-export default App;
