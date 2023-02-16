41e47f2e6dc084c02e9ae324db755b7fbf6354a1
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-16 14:30:30 +0200

    OS page working

--- src/components/EditFormOS.js
+++ src/components/EditFormOS.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { TextField, Button, Box } from "@material-ui/core";
-import { AddCircle } from "@material-ui/icons";
+import { TextField, Button, Grid } from "@material-ui/core";
 import "../components/styles/forms.css"
 
-function EditFormOS() {
-  const [name, setName] = useState("");
-  const [releasedYear, setReleasedYear] = useState("");
+function EditFormPL(params) {
+  const [dataId] = useState(params.params.row.id);
+  const [name, setName] = useState(params.params.row.name);
+  const [releasedYear, setReleasedYear] = useState(params.params.row.released_year);
 
   const handleAddLanguage = async (event) => {
     event.preventDefault();
@@ -16,8 +16,8 @@ function EditFormOS() {
     };
 
     try {
-      const response = await fetch("http://localhost:3001/OS/new", {
-        method: "POST",
+      const response = await fetch("http://localhost:3001/OS/edit/" + dataId, {
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
@@ -32,35 +32,34 @@ function EditFormOS() {
 
   return (
     <form onSubmit={handleAddLanguage}>
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <TextField
-          label="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          margin="normal"
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          label="Released Year"
-          type="number"
-          value={releasedYear}
-          onChange={(e) => setReleasedYear(e.target.value)}
-          margin="normal"
-          variant="outlined"
-          size="small"
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          startIcon={<AddCircle />}
-        >
-          Edit Operating System
-        </Button>
-      </Box>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Released Year"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={releasedYear}
+            onChange={(e) => setReleasedYear(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary" type="submit">
+            Edit Language
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 }
 
-export default EditFormOS;
+export default EditFormPL;
