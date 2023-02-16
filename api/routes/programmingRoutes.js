a0617af04a54f0285a88d2c135e8a258c63d8824
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-16 10:40:03 +0200

    small errors in routing

--- api/routes/programmingRoutes.js
+++ api/routes/programmingRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const programmingLanguages = require("../services/programmingLanguages");
+const opSys = require("../services/opSys");
+const eligibility = require("../services/eligibility");
 
 /* GET programming languages. */
 router.get("/pl/list", async function (req, res, next) {
@@ -54,5 +56,75 @@ router.delete("/pl/delete/:id", async function (req, res, next) {
     next(err);
   }
 });
+
+// OPERATING SYSTEMS 
+
+/* GET operating systems. */
+router.get("/OS/list", async function (req, res, next) {
+  try {
+    res.json(await opSys.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting operating systems `, err.message);
+    next(err);
+  }
+});
+
+/* POST operating systems */
+router.post("/OS/new", async function (req, res, next) {
+  try {
+    res.json(await opSys.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating operating system`, err.message);
+    next(err);
+  }
+});
+
+/* SEARCH operating systems */
+
+router.get("/search/OS/:key", async function (req, res, next) {
+    try {
+    res.json(await opSys.search(req.params.key));
+  } catch (err) {
+    console.error(`Error while finding opearting system`, err.message);
+    next(err);
+  }
+});
+
+/* PUT operating systems */
+router.put("/OS/edit/:id", async function (req, res, next) {
+  try {
+    res.json(await opSys.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating system`, err.message);
+    next(err);
+  }
+});
+
+/* DELETE operating systems */
+router.delete("/OS/delete/:id", async function (req, res, next) {
+  try {
+    //res.json(await programmingLanguages.remove(req.params.id)); and also change the "/:id" in the router.detlete()- use this if you dont want to use the UI of POSTMAN
+    res.json(await opSys.remove(req.query.id));
+
+  } catch (err) {
+    console.error(`Error while deleting operating system`, err.message);
+    next(err);
+  }
+});
+
+// Eligibility
+
+/* SEARCH for eligibility */
+
+router.get("/eligibility/:key/:value", async function (req, res, next) {
+  try {
+    res.json(await eligibility.search(req.params.key, req.params.value));
+  } catch (err) {
+    console.error(`Not compatible`, err.message);
+    next(err);
+  }
+});
+
+
 
 module.exports = router;
