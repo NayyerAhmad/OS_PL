b40db452c9f2a64de2942fa5a7714229e1498a5a
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-17 02:45:39 +0200

    editing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,66 +1,8 @@
-import React, { useState } from "react";
-import { TextField, Button, Box } from "@material-ui/core";
-import { AddCircle } from "@material-ui/icons";
-import "../components/styles/forms.css"
+import '../components/styles/home.css'
 
-function FormCompatibility() {
-  const [nameOS, setNameOS] = useState("");
-  const [namePL, setNamePL] = useState("");
-
-  const handleAddLanguage = async (event) => {
-    event.preventDefault();
-
-    const data = {
-      name_os: nameOS,
-      name_pl: namePL,
-    };
-
-    try {
-      const response = await fetch("http://localhost:3001/eligibility/new", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      const responseData = await response.json();
-      console.log(responseData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <form onSubmit={handleAddLanguage}>
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <TextField
-          label="Name"
-          value={nameOS}
-          onChange={(e) => setNameOS(e.target.value)}
-          margin="normal"
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          label="Released Year"
-          type="number"
-          value={namePL}
-          onChange={(e) => setNamePL(e.target.value)}
-          margin="normal"
-          variant="outlined"
-          size="small"
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          startIcon={<AddCircle />}
-        >
-          Add Relationship
-        </Button>
-      </Box>
-    </form>
-  );
+function About() {
+  return <p>A simple app made with React as front-end and Express as back-end.
+    The app shows the list of all the operating systems and the programming languages in the database. Additionally it allows you to check the compatibility depending on your operating system and the programming language
+  </p>;
 }
-
-export default FormCompatibility;
+export default About;
